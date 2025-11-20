8815f0942dfac6d67402babc58f1c55a7fec51c9
HasanKaranouh <hkaranouh2004@gmail.com>
2025-11-20 17:03:31 +0200

    feat: Add initial typing test screen
    Refactors the initial app structure by:
    - Renaming the main `MyApp` widget to `TypingSpeedApp`.
    - Creating a new stateful widget, `TypingTestScreen`, as the home screen.
    - Updating `main.dart` to use `TypingTestScreen` and setting the app title.
    - Adjusting the widget test to reflect the app widget rename.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'screens/typing_test_screen.dart';
 
-    void main() =>runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() {
+  runApp(const TypingSpeedApp());
+}
+
+class TypingSpeedApp extends StatelessWidget {
+  const TypingSpeedApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-
+      title: "Typing Speed Tester",
+      debugShowCheckedModeBanner: false,
+      home: const TypingTestScreen(),
     );
   }
 }
---
+++ lib/screens/typing_test_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TypingTestScreen extends StatefulWidget {
+  const TypingTestScreen({super.key});
+
+  @override
+  State<TypingTestScreen> createState() => _TypingTestScreenState();
+}
+
+class _TypingTestScreenState extends State<TypingTestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
