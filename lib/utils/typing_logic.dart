187b5220e2f49e7c21c6cb4d844571beaa69b0d4
HasanKaranouh <hkaranouh2004@gmail.com>
2025-11-21 16:40:35 +0200

    feat: Define TypingResult data model
    Populates the `TypingResult` class with the data structure required to store the outcome of a typing test.
    
    This includes:
    - Adding final properties for `seconds`, `wpm`, `accuracy`, and a `message`.
    - Creating a constructor to initialize these properties.

--- lib/utils/typing_logic.dart
+++ lib/utils/typing_logic.dart
@@ -1,5 +1,15 @@
 class TypingResult{
+  final double seconds;
+  final double wpm;
+  final double accuracy;
+  final String message;
 
+  TypingResult({
+    required this.seconds,
+    required this.wpm,
+    required this.accuracy,
+    required this.message,
+});
 }
 class TypingLogic{
 
