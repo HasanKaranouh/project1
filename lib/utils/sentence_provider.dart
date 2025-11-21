a89bdb5266fd4b5352458782f49927200d728257
HasanKaranouh <hkaranouh2004@gmail.com>
2025-11-21 16:33:04 +0200

    feat: Add sentences for typing test
    This commit populates the `SentenceProvider` with a static list of sentences to be used in the typing test.

--- lib/utils/sentence_provider.dart
+++ lib/utils/sentence_provider.dart
@@ -1,5 +1,10 @@
 import 'dart:math';
 
 class SentenceProvider{
-
+  static final List<String> sentences = [
+    "The quick brown foc jumps over the lazy dog.",
+    "Flutter makes it easy to build beautiful apps.",
+    "Typing fast is a useful skill for everyone.",
+    "Practice every day to improve your typing speed.",
+  ];
 }
